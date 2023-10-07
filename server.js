69e0d042932798afbd10ee0890e0ea05d573ad7f
lightyagami1702 <69008354+lightyagami1702@users.noreply.github.com>
2023-10-07 21:43:57 +0530

    Updated server.js
    dirname updated

--- server.js
+++ server.js
@@ -7,11 +7,15 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from "path";
+import {fileURLToPath} from 'url';
 //env config
 dotenv.config();
 
 //databaseConfig
 connectDB();
+//file to path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 //app config
 const app = express();
 
